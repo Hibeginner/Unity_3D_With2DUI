68303c8d6941a86a2385fdb6a185d9db2617884c
Pang Xiang Yu <501997064@qq.com>
2016-09-07 20:57:35 +0800

    Whole project.

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+    [SerializeField] private Text scoreLabel;
+    [SerializeField] private SettingsPopup settingsPopup;
+
+    private int _score;
+
+    void Awake() {
+        Messenger.AddListener(GameEvent.ENEMY_HIT, OnEnemyHit);
+    }
+
+    void OnDestroy() {
+        Messenger.RemoveListener(GameEvent.ENEMY_HIT, OnEnemyHit);
+    }
+
+	// Use this for initialization
+	void Start () {
+        _score = 0;
+        scoreLabel.text = _score.ToString();
+
+        settingsPopup.Close();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void OnEnemyHit() {
+        _score += 1;
+        scoreLabel.text = _score.ToString();
+    }
+
+    public void OnOpenSettings() {
+        settingsPopup.Open();
+    }
+
+    public void OnPointerDown() {
+        Debug.Log("down");
+    }
+}
